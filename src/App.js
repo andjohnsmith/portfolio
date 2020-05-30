2a056f9cba8853804cfa84e5be48b1b86fcfa88c
Andrew Smith <smithandrewj@outlook.com>
2020-05-30 11:51:06 -0700

    style changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import Head from './components/head.jsx';
 import About from './components/about.jsx';
 import Projects from './components/projects.jsx';
 import Work from './components/work.jsx';
 import Contact from './components/contact.jsx';
 import Foot from './components/foot.jsx';
+
+import './App.css';
 
 function App() {
   return (
