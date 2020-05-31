0688529fdd87189b3211776ac9b6fba2155f0c85
Andrew Smith <smithandrewj@outlook.com>
2020-05-31 13:04:30 -0700

    added useEffect to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Navbar = () => {
+  const [data, setData] = useState({
+    isOpen: false,
+    isScrolling: false,
+  });
+
+  useEffect(() => {
+    window.addEventListener('scroll', scroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', scroll);
+    };
+  });
+
+  const toggle = () => {
+    setData({
+      ...data,
+      isOpen: !data.isOpen,
+    });
+  };
+
+  const scroll = () => {
+    setData({
+      ...data,
+      isScrolling: window.pageYOffset > 50 ? true : false,
+    });
+  };
+
   return (
-    <div id="fixed-header" className="navbar navbar-expand-md navbar-light">
+    <div
+      id="fixed-header"
+      className={
+        'navbar navbar-expand-md navbar-light' +
+        (data.isScrolling ? ' scroll' : '')
+      }
+    >
       <div className="container">
         <a className="navbar-brand" href="#about">
           andjohnsmith
         </a>
-        <button type="button" className="navbar-toggler">
+        <button type="button" className="navbar-toggler" onClick={toggle}>
           <span className="navbar-toggler-icon" />
         </button>
-        <div id="main-menu" className="collapse navbar-collapse">
+        <div
+          id="main-menu"
+          className={'collapse navbar-collapse' + (data.isOpen ? ' show' : '')}
+        >
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link" href="#about">
